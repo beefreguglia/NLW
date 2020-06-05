4cc83e46fa44e06966cd77fc357f0f686af319a9
Bernardo <beefreguglia@gmail.com>
2020-06-05 16:26:36 -0300

    Criado o banco de dados pelo sqlite3 e preparando o background para a aplicação por com o o banco e deixando tudo dinâmico e funcional

--- src/server.js
+++ src/server.js
@@ -3,8 +3,14 @@ const express = require("express")
 //server executa o express
 const server = express()
 
+//pegar banco de dados
+const db = require("./database/db")
+
 //configurar a pasta publica
 server.use(express.static("public"))
+
+//habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true}))
 
 //Utilizando a template engine
 const nunjucks = require("nunjucks")
@@ -29,13 +35,93 @@ server.get("/", (req, res) => {
 
 server.get("/create-point", (req, res) => {
 
-   return res.render("create-point.html")
+    //req.query Query String da nossa url 
+    //console.log(req.query)
+
+   return res.render("create-point.html")   
+
+})
+
+server.post("/save-point", (req, res) =>{
+
+    //req.body: O corpo do nosso formulario
+    console.log(req.body)
+
+    //inserir dados no banco de dados
+
+     const query = `
+        
+     INSERT INTO places (
+
+         image,
+         name,
+         address,
+         address2,
+         state,
+         city,
+         items
+
+     ) VALUES ( ?,?,?,?,?,?,? );`
+
+     const values = [
+ 
+         req.body.image,
+         req.body.name,
+         req.body.address,
+         req.body.address2,
+         req.body.state,
+         req.body.city,
+         req.body.items
+ 
+     ]
+
+     function afterInsertData(err){
+
+         if(err){
+
+            console.log(err)
+            return res.render("create-point.html",{ wrong: true })
+         
+        }
+
+         console.log("Cadastrado com sucesso")
+         console.log(this)
+
+         return res.render("create-point.html",{ saved: true })
+
+     }
+
+ db.run(query, values, afterInsertData)
 
 })
 
 server.get("/search", (req, res) => {
 
-    return res.render("search-results.html")
+    const search = req.query.search
+
+    if(search == ""){
+    
+        return res.render("search-results.html", { total: 0 })
+    
+    }
+
+
+    //consultar dados
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+            
+        if(err){
+
+            return console.log(err)
+        
+        }
+
+        const total = rows.length
+
+        //mostra a pagina html com o banco de dados
+        return res.render("search-results.html", {places: rows, total: total})
+      
+    })
  
  })
 
